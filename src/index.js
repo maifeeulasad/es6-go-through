8e472e0023c6bcab38a203a7da3588b23cb06b83
Maifee Ul Asad <maifeeulasad@gmail.com>
2022-01-23 22:00:45 +0600

    string templating

--- src/index.js
+++ src/index.js
@@ -1,42 +1,19 @@
-// Variable declaration
+console.log('hello world')
 
-// var a = "abc"
-// console.log(a)
+console.log('hello' + ' \n' + 'world')
 
-// const pi = 3.14
-// let c = 5
-
-// console.log(pi)
-// console.log(c)
-
-// We can't change the value for `const`
-// pi = 3.14159
-// c = 5.1
-// console.log(c)
-
-/*
-var a = "a-----"
-
-{
-    var b="b---------"
-    console.log(a)
-    console.log(b)
-}
-console.log(a)
-console.log(b)
-*/
+console.log("hello " +
+    "world")
 
 
-/*
-let a = "a++++++"
+console.log(`hello world`)
 
-{
-    let b = "b++++++++++++++++++"
-    console.log(a)
-    console.log(b)
-}
 
-console.log(a)
-//console.log(b)
 
- */
+const x = 5
+
+
+console.log(`hello world${x}`)
+console.log('hello world'+x)
+
+console.log(`Age is greater than 5 ${x>=5}`)
